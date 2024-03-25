51ca78e3738cbb38a6139674001f8090ab6c2b6f
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-03-25 08:34:17 +0700

    Create B. Variables.php

---
+++ B. Variables.php
@@ -0,0 +1,37 @@
+<?php
+# This is php code without html
+
+// Variables start with a dollar sign
+
+$x = 18;
+$y = "Ryufath";
+
+echo "Hey, my name is $y I am $x"
+
+#print sum
+
+$z = 10;
+
+echo $x + $y;
+
+// Integer
+$intVar = 10;
+
+// Float
+$floatVar = 3.14;
+
+// String
+$stringVar = "Hello, world!";
+
+// Boolean
+$boolVar = true;
+
+// Array
+$arrayVar = array(1, 2, 3);
+
+// Null
+$nullVar = null;
+
+var_dump(floatVar); // Returns the data value type
+
+?>
