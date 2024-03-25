d7a866b7ab32e1dc9e1610e2153c8f6f22128e6f
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-03-25 08:23:20 +0700

    Update A. Intro.php

--- A. Intro.php
+++ A. Intro.php
@@ -3,6 +3,14 @@
 <body>
  
 <?php
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+
 echo "Hello, this is my first PHP script!";
 ?>
 
