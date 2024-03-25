460713881f1a19f1177f87c454d7f7f2cda94c95
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-03-25 09:03:38 +0700

    Create E. Math.php

---
+++ E. Math.php
@@ -0,0 +1,19 @@
+<?php
+echo(min(0, 150, 30, 20, -8, -200) . "<br>");
+echo(max(0, 150, 30, 20, -8, -200));
+
+echo(pi() . "<br>"); // Returns pi
+
+echo(sqrt(64). "<br>");
+
+echo(abs(-6.7). "<br>"); // Returns absolute positive
+
+echo(rand() . "<br>"); // Random number
+
+echo(round(0.60) . "<br>");
+echo(round(0.50) . "<br>");
+echo(round(0.49) . "<br>");
+echo(round(-4.40) . "<br>");
+echo(round(-4.60));
+
+?>
